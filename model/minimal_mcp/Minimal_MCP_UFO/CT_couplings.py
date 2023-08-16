5e4a6953a12dab55a4254fc657a68872548190b2
Insung Hwang <his5624@korea.ac.kr>
2023-08-16 14:38:05 +0900

    Add feynRules minimal MCP model

---
+++ model/minimal_mcp/Minimal_MCP_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.0.0 for Linux x86 (64-bit) (December 3, 2021)
+# Date: Mon 14 Aug 2023 02:49:50
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ model/minimal_mcp/Minimal_MCP_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.0.0 for Linux x86 (64-bit) (December 3, 2021)
+# Date: Mon 14 Aug 2023 02:49:50
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
